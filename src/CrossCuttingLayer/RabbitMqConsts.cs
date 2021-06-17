19999de640fdfe6a2d2c3a5f40d0c48fad8973e0
Amit P Naik <amit.naik8103@gmail.com>
2021-06-17 11:22:32 +0530

    feat: adding const

---
+++ src/CrossCuttingLayer/RabbitMqConsts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossCuttingLayer
+{
+    public class RabbitMqConsts
+    {
+        public const string RabbitMqUri = "rabbitmq://localhost/todoqueue/";
+        public const string UserName = "guest";
+        public const string Password = "guest";
+        public const string RegisterTodoServiceQueue = "registerdemand.service";
+        public const string NotificationServiceQueue = "notification.service";
+        public const string ThirdPartyServiceQueue = "thirdparty.service";
+    }
+}
