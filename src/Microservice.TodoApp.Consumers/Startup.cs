e78df3bc3fabbaea3b341ca4ff8f5efb882ff64e
Amit P Naik <amit.naik8103@gmail.com>
2021-06-17 11:17:31 +0530

    refactor: removed whitespace

--- src/Microservice.TodoApp.Consumers/Startup.cs
+++ src/Microservice.TodoApp.Consumers/Startup.cs
@@ -2,17 +2,11 @@ using GreenPipes;
 using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Microservice.TodoApp.Consumers
 {
--- src/Microservice.TodoApp.Publisher/Controllers/TodoController.cs
+++ src/Microservice.TodoApp.Publisher/Controllers/TodoController.cs
@@ -2,9 +2,6 @@
 using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Microservice.TodoApp.Publisher.Controllers
